f318f25419089598bd4080377ffa9055cdcb1fa7
Henrique <ishenriquealbert@gmail.com>
2021-03-09 20:45:22 -0300

    updated salesstatus

--- src/components/SalesStatus/index.js
+++ src/components/SalesStatus/index.js
@@ -1,10 +1,8 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
+import { formatPrice } from 'utils/formatPrice';
 
-export default function SalesStatus() {
-  const aprovadas = 'R$ 1.400,00';
-  const canceladas = 'R$ 440,00';
-  const pendentes = 'R$ 600,00';
-
+export default function SalesStatus({ data }) {
+  console.log(data);
   return (
     <Box
       w="100%"
@@ -14,65 +12,38 @@ export default function SalesStatus() {
       p={{ base: '1.5rem', xxl: '2rem' }}
       mb={{ base: '1.5rem', xlg: '0' }}
     >
-      <Flex justifyContent="space-between">
-        <Text
-          as="h4"
-          color="gray.500"
-          fontSize={{ base: '1rem', xxl: '1.25rem' }}
-          lineHeight="1.5rem"
-        >
-          Aprovadas
-        </Text>
-        <Text
-          as="span"
-          color="gray.500"
-          fontWeight="bold"
-          fontSize={{ base: '1.15rem', xxl: '1.375rem' }}
-          lineHeight="1.688rem"
-        >
-          {aprovadas}
-        </Text>
-      </Flex>
-      <Box h="0.063rem" bg="#ECECEC" m={{ base: '0.75rem', xxl: '1rem' }} />
-      <Flex justifyContent="space-between">
-        <Text
-          as="h4"
-          color="gray.500"
-          fontSize={{ base: '1rem', xxl: '1.25rem' }}
-          lineHeight="1.5rem"
-        >
-          Canceladas
-        </Text>
-        <Text
-          as="span"
-          color="gray.500"
-          fontWeight="bold"
-          fontSize={{ base: '1.15rem', xxl: '1.375rem' }}
-          lineHeight="1.688rem"
-        >
-          {canceladas}
-        </Text>
-      </Flex>
-      <Box h="0.063rem" bg="#ECECEC" m={{ base: '0.75rem', xxl: '1rem' }} />
-      <Flex justifyContent="space-between">
-        <Text
-          as="h4"
-          color="gray.500"
-          fontSize={{ base: '1rem', xxl: '1.25rem' }}
-          lineHeight="1.5rem"
-        >
-          Pendentes
-        </Text>
-        <Text
-          as="span"
-          color="gray.500"
-          fontWeight="bold"
-          fontSize={{ base: '1.15rem', xxl: '1.375rem' }}
-          lineHeight="1.688rem"
-        >
-          {pendentes}
-        </Text>
-      </Flex>
+      {data?.map((item) => (
+        <Box key={item?.label}>
+          <Flex justifyContent="space-between">
+            <Text
+              as="h4"
+              color="gray.500"
+              fontSize={{ base: '1rem', xxl: '1.25rem' }}
+              lineHeight="1.5rem"
+            >
+              {item?.label}
+            </Text>
+            <Text
+              as="span"
+              color="gray.500"
+              fontWeight="bold"
+              fontSize={{ base: '1.15rem', xxl: '1.375rem' }}
+              lineHeight="1.688rem"
+            >
+              {formatPrice(item?.value)}
+            </Text>
+          </Flex>
+          {item?.label === 'Pendentes' ? (
+            ''
+          ) : (
+            <Box
+              h="0.063rem"
+              bg="#ECECEC"
+              m={{ base: '0.75rem', xxl: '1rem' }}
+            />
+          )}
+        </Box>
+      ))}
     </Box>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,9 +10,21 @@ import SalesByIssuer from 'components/SalesByIssuer';
 import InnerMenu from 'components/InnerMenu';
 
 import { useHomeContext } from './HomeContext';
+import useStatisticsHome from 'hooks/useStatisticsHome';
 
 export default function Home() {
   const ctx = useHomeContext();
+
+  const { data } = useStatisticsHome('2020-01-01', '2020-12-31');
+
+  console.log(data);
+
+  // todo:
+  // sale status
+  // sales percentages
+  // sales credit
+  // sales by issuer
+  // filter start date & end date
 
   return (
     <Layout>
@@ -20,30 +32,32 @@ export default function Home() {
         <Box mb="3.25rem">
           <InnerMenu pageTitle="Home" useContext={ctx} />
 
-          <Box bg="gray.200" ref={ctx.printRef}>
-            <Box
-              display={{ base: 'block', xl: 'grid' }}
-              gridTemplateColumns={{ xl: '1fr 1fr', xxl: '2fr 1fr' }}
-              gridGap={{ xl: '1.313rem' }}
-              w="100%"
-              mt="0.5rem"
-            >
-              <SalesStatus />
-              <SalesPercentages />
-            </Box>
-            <Box
-              display={{ base: 'block', xxl: 'grid' }}
-              gridTemplateColumns={{ xxl: '2fr 1fr' }}
-              gridGap={{ xxl: '1.313rem' }}
-              w="100%"
-            >
-              <Box>
-                <SalesCredit />
-                <SalesByHour />
+          {data && (
+            <Box bg="gray.200" ref={ctx.printRef}>
+              <Box
+                display={{ base: 'block', xl: 'grid' }}
+                gridTemplateColumns={{ xl: '1fr 1fr', xxl: '2fr 1fr' }}
+                gridGap={{ xl: '1.313rem' }}
+                w="100%"
+                mt="0.5rem"
+              >
+                <SalesStatus data={data?.sales_status} />
+                <SalesPercentages />
               </Box>
-              <SalesByIssuer />
+              <Box
+                display={{ base: 'block', xxl: 'grid' }}
+                gridTemplateColumns={{ xxl: '2fr 1fr' }}
+                gridGap={{ xxl: '1.313rem' }}
+                w="100%"
+              >
+                <Box>
+                  <SalesCredit />
+                  <SalesByHour />
+                </Box>
+                <SalesByIssuer />
+              </Box>
             </Box>
-          </Box>
+          )}
         </Box>
       </Container>
     </Layout>
